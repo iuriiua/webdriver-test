4ca6aa7d0e1e9e7f8ca81b19bf82994caa5820c2
iuriisokolov <iurii.sokolov@gmail.com>
2019-08-11 16:27:21 -0700

    afaf

--- test/test.js
+++ test/test.js
@@ -2,17 +2,21 @@ import { assert } from 'chai';
 
 describe('Client', function () { //define suite title by passing a string
 
-    it('Get title', function () { //define test title by passing a string
+    it('Page title', function () { //define test title by passing a string
       browser.url('/'); //open baseUrl + string passed in .url() function
       let title = browser.getTitle(); //get page title and assign it to the "title" variable
       browser.pause(2000); //just pause to visually see that something is happening on the page
       assert.equal(title, 'Progress Monitor'); //compare {title} (actual) and "Bug Tracker" (expected)
     })
 
-    it('URL', function () { //define test title by passing a string
-      let title = $('#site-name').getText(); //get page title and assign it to the "title" variable
-      console.log(title);
-      //assert.equal(title, 'Progress Monitor'); //compare {title} (actual) and "Bug Tracker" (expected)
+    it('Title and version', function () {
+      let title = $('#site-name').getText();
+      assert.equal(title, 'Progress Monitor 0.1.61');
+    })
+
+    it('Title and version', function () {
+      let title = $('#site-name').getText();
+      assert.equal(title, 'Progress Monitor 0.1.61');
     })
     
 });
